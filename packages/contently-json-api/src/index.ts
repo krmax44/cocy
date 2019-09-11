64d096adf411d6488aba71d93bb53bef791779dd
krmax44 <hi@krmax44.de>
2019-09-11 18:04:02 +0200

    fix: module export

--- packages/contently-json-api/src/index.ts
+++ packages/contently-json-api/src/index.ts
@@ -4,4 +4,6 @@ export default ContentlyJsonApi;
 
 if (typeof module !== 'undefined') {
 	module.exports = ContentlyJsonApi;
+	module.exports.default = ContentlyJsonApi;
+	module.exports.ContentlyJsonApi = ContentlyJsonApi;
 }
