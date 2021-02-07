e850fd348d1185cc655f42d957aa42358c2b2531
krmax44 <hi@krmax44.de>
2021-02-07 22:15:17 +0100

    fix: markdown test

--- packages/contently-transform-markdown/src/tests/basic.test.ts
+++ packages/contently-transform-markdown/src/tests/basic.test.ts
@@ -12,10 +12,10 @@ describe('ContentlyTransformMarkdown', () => {
 		const second = path.join(cwd, 'Second-Post.md');
 		const third = path.join(cwd, 'Third-Post.md');
 
-		expect(contently.files.get(first).data).toBe(`<h1>Hello World</h1>
-<p>Hello from Contently!</p>
-<!-- more -->
-<p>Lorem ipsum</p>
-<p><img src="/home/max/Dokumente/Projects/contently-new/packages/contently-transform-markdown/src/tests/fixtures/basic/Hello-World.md/test.png" alt="Image"></p>\n`);
+		const secondFile = contently.files.get(second);
+		expect(secondFile.data).toBe(
+			'<h1>Second Post</h1>\n<p>This is the second post!</p>\n'
+		);
+		expect(secondFile.attributes.excerpt).toBe('Second post!');
 	});
 });
